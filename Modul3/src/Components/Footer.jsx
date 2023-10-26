173750dc1fe7f93046805dc3d0c471d13b2e9f87
Muflihys <119804435+Muflihys@users.noreply.github.com>
2023-10-26 08:23:35 +0700

    Delete Modul3 directory

--- Modul3/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer= () => {
-    return(
-        <>
-            <p1>Footer</p1>
-        </>
-    )
-}
-
-export default Footer
--- Modul3/src/Components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-const Hero = () => {
-    return (
-        <>
-            <h3>Welcome</h3>
-        </>
-    )
-}
-
-export default Hero
