b603ef8d79f1124cf2a316ed35292f3b7f2543ac
Muflihys <119804435+Muflihys@users.noreply.github.com>
2023-10-26 08:19:49 +0700

    Add files via upload

---
+++ Modul3/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer= () => {
+    return(
+        <>
+            <p1>Footer</p1>
+        </>
+    )
+}
+
+export default Footer
---
+++ Modul3/src/Components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <>
+            <h3>Welcome</h3>
+        </>
+    )
+}
+
+export default Hero
